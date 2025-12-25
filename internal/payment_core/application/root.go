0366ad40b8a4943f310ea2d53f3e1476bdd22ad0
Jerry <midsmr@qq.com>
2025-12-25 13:41:34 +0800

    feat: bootstrap application

---
+++ internal/payment_core/application/root.go
@@ -0,0 +1,52 @@
+package application
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/midsmr/payment/pkg/payment_logger"
+)
+
+func Init() error {
+
+	if err := initRootPath(); err != nil {
+		return err
+	}
+
+	if err := initLogger(); err != nil {
+		return err
+	}
+
+	Logger.Info("Application running in root path: " + RootPath)
+
+	Logger.Info("Application initialized successfully.")
+
+	return nil
+}
+
+func initRootPath() error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	RootPath = cwd
+	return nil
+}
+
+func initLogger() error {
+	path := filepath.Join(RootPath, "logs")
+
+	log, err := payment_logger.Init(path)
+
+	if err != nil {
+		return err
+	}
+
+	Logger = log
+
+	Logger.Info("Initializing application...")
+
+	Logger.Info("Logger initialized successfully. File path： " + path)
+
+	return nil
+}
---
+++ internal/payment_core/application/runtime.go
@@ -0,0 +1,15 @@
+package application
+
+import (
+	"github.com/midsmr/payment/pkg/payment_logger"
+)
+
+var RootPath string
+
+var Logger = payment_logger.NewConsoleLogger()
+
+var CoreVersion = "dev"
+
+var LastCommit = "unknown"
+
+var BuildDate = "unknown"
